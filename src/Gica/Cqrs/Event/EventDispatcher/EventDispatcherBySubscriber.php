c17053ceed1446d8a8afe7553717f63cb1bc03cf
Constantin GALBENU <gica.galbenu@gmail.com>
2017-07-04 10:00:13 +0300

    wrapped event dispatching in try/catch for \Gica\Cqrs\Event\EventDispatcher\EventDispatcherBySubscriber

--- src/Gica/Cqrs/Event/EventDispatcher/EventDispatcherBySubscriber.php
+++ src/Gica/Cqrs/Event/EventDispatcher/EventDispatcherBySubscriber.php
@@ -9,17 +9,23 @@ namespace Gica\Cqrs\Event\EventDispatcher;
 use Gica\Cqrs\Event\EventDispatcher;
 use Gica\Cqrs\Event\EventSubscriber;
 use Gica\Cqrs\Event\EventWithMetaData;
+use Psr\Log\LoggerInterface;
 
 class EventDispatcherBySubscriber implements EventDispatcher
 {
     /** @var EventSubscriber */
     private $eventSubscriber;
 
+    /** @var LoggerInterface|null */
+    private $logger;
+
     public function __construct(
-        EventSubscriber $eventSubscriber
+        EventSubscriber $eventSubscriber,
+        LoggerInterface $logger = null
     )
     {
         $this->eventSubscriber = $eventSubscriber;
+        $this->logger = $logger;
     }
 
     public function dispatchEvent(EventWithMetaData $eventWithMetadata)
@@ -27,7 +33,19 @@ class EventDispatcherBySubscriber implements EventDispatcher
         $listeners = $this->eventSubscriber->getListenersForEvent($eventWithMetadata->getEvent());
 
         foreach ($listeners as $listener) {
-            call_user_func($listener, $eventWithMetadata->getEvent(), $eventWithMetadata->getMetaData());
+            try {
+                call_user_func($listener, $eventWithMetadata->getEvent(), $eventWithMetadata->getMetaData());
+            } catch (\Throwable $exception) {
+                if ($this->logger) {
+                    $this->logger->error(
+                        sprintf(
+                            "Dispatch event of type %s to %s failed: %s",
+                            get_class($eventWithMetadata->getEvent()),
+                            get_class($listener[0]),
+                            $exception->getMessage()),
+                        $exception->getTrace());
+                }
+            }
         }
     }
 }
--- tests/Gica/Cqrs/Event/EventDispatcher/EventDispatcherBySubscriberTest.php
+++ tests/Gica/Cqrs/Event/EventDispatcher/EventDispatcherBySubscriberTest.php
@@ -8,6 +8,8 @@ use Gica\Cqrs\Event;
 use Gica\Cqrs\Event\EventDispatcher\EventDispatcherBySubscriber;
 use Gica\Cqrs\Event\EventSubscriber;
 use Gica\Cqrs\Event\EventWithMetaData;
+use Gica\Cqrs\Event\MetaData;
+use Psr\Log\LoggerInterface;
 
 
 class EventDispatcherBySubscriberTest extends \PHPUnit_Framework_TestCase
@@ -44,16 +46,60 @@ class EventDispatcherBySubscriberTest extends \PHPUnit_Framework_TestCase
         $sut->dispatchEvent($eventWithMetaData);
     }
 
+    public function test_exception_is_catched()
+    {
+        $eventWithMetaData = new EventWithMetaData(
+            $this->mockEvent(),
+            $this->mockMetadata()
+        );
+
+        $listener = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['onEvent1'])
+            ->getMock();
+        $listener->expects($this->once())
+            ->willThrowException(new \Exception("test exception"))
+            ->method('onEvent1');
+
+        $logger = $this->getMockBuilder(LoggerInterface::class)
+            ->getMock();
+        $logger->expects($this->once())
+            ->method('error');
+
+
+        $eventSubscriber = $this->getMockBuilder(EventSubscriber::class)
+            ->getMock();
+
+        $eventSubscriber->expects($this->once())
+            ->method('getListenersForEvent')
+            ->willReturn([
+                [$listener, 'onEvent1'],
+            ]);
+
+        /** @var  EventSubscriber $eventSubscriber */
+        $sut = new EventDispatcherBySubscriber(
+            $eventSubscriber,
+            $logger
+        );
+
+        $sut->dispatchEvent($eventWithMetaData);
+    }
+
     private function mockEvent()
     {
         return $this->getMockBuilder(Event::class)
             ->getMock();
     }
 
+    /**
+     * @return MetaData
+     */
     private function mockMetadata()
     {
-        return $this->getMockBuilder(Event\MetaData::class)
+        $metadata = $this->getMockBuilder(MetaData::class)
             ->disableOriginalConstructor()
             ->getMock();
+
+        /** @var MetaData $metadata */
+        return $metadata;
     }
 }
